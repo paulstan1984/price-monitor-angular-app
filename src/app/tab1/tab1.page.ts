e67fe991fb731ed9e5c299c1f73f25b6ed1fa68e
Paul Stan <Paul Stan>
2021-01-29 17:15:37 +0200

    Update VSWorkspaceState.json, slnx.sqlite, and 64 more files...

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private speechRecognition: SpeechRecognition) { }
 
+  words: string = '';
+
+  public VoiceToText() {
+
+
+    // Start the recognition process
+    this.speechRecognition.startListening()
+      .subscribe(
+        (matches: string[]) => {this.words += matches.join(' ')},
+        (onerror) => console.log('error:', onerror)
+      )
+
+    // Stop the recognition process (iOS only)
+    //this.speechRecognition.stopListening()
+
+   
+  }
 }
