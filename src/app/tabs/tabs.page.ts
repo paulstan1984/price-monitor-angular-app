8565e0a6726a2a5ea758dd85df72e3e41ede3e6b
Paul Stan <Paul Stan>
2021-02-04 16:58:26 +0200

    Update tabs.page.html and tabs.page.ts

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Plugins } from '@capacitor/core';
 import { AlertController } from '@ionic/angular';
+import { BaseComponent } from '../BaseComponent';
+import { Price } from '../models/Price';
 const { App } = Plugins;
 
 @Component({
@@ -8,9 +10,11 @@ const { App } = Plugins;
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {
+export class TabsPage extends BaseComponent {
 
-  constructor(private alertController: AlertController) {}
+  constructor(private alertController: AlertController) {
+    super();
+  }
 
   async Exit() {
     const alert = await this.alertController.create({
@@ -25,5 +29,42 @@ export class TabsPage {
     });
 
     alert.present();
+  }
+
+  async Buy() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Buy?',
+      buttons: [{
+        text: 'Yes',
+        handler: () => {
+          this.BuyShoppingList();
+        }
+      }, 'No']
+    });
+
+    alert.present();
+  }
+
+  BuyShoppingList() {
+    let list = this.getShoppingList();
+    let store_id = parseInt(this.getCurrentStore());
+    let prices = [];
+
+    if (list && list.items && list.items.length > 0) {
+      list.items.forEach(item => {
+        let price = {
+          product_id: item.product.id,
+          store_id: store_id,
+          amount: item.price
+        } as Price;
+
+        if (item.checked) {
+          prices.push(price);
+        }
+      });
+    }
+
+    
   }
 }
