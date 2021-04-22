7dc3b67b8e22d46c2743bd312b411ceb3d2bccd3
Paul Stan <Paul Stan>
2021-04-22 14:50:37 +0300

    Update statistics.component.html and statistics.component.ts

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,4 +1,4 @@
-import { Component, Injector, OnInit } from '@angular/core';
+import { Component, Injector } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { BaseComponent } from '../BaseComponent';
 import { StatisticsRequest, StatisticsValue } from '../models/StatisticsRequest';
